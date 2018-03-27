960d0cd191c60a68c3083786a25a4298a7a8968a
Patrisha-de-Boon <32069658+Patrisha-de-Boon@users.noreply.github.com>
2018-03-27 08:49:18 -0600

    tweaked minor values

--- shared.py
+++ shared.py
@@ -5,6 +5,5 @@ height = 700
 imgWidth = 39
 imgHeight = 43
 step = 10  # step is the number of pixels the character moves each key press
-fps = 40
-acycle = 3 # number of images per animation cycle
+fps = 60
 clock = pygame.time.Clock()
