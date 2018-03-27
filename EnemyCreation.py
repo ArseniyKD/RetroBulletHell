3f24da7e480f5539b31e55cc358cbc34b60fd2a5
ArseniyKD <arseniy.kouzmenkov@gmail.com>
2018-03-27 15:15:16 -0600

    updated the enemy creation file and added a metric crapton of getters and setters

--- EnemyCreation.py
+++ EnemyCreation.py
@@ -1,10 +1,12 @@
 from avl_dict import AVLDict as aDict
-from uuid import uuid4
 from random import *
 
 class Enemy:
     def __init__(self, Etype):
         self.Etype = Etype
+        self.imageSize = 24
+        self.healthValue = 0
+        self.reDraw = True
 
     def Einfo(self):
         if self.Etype == "Common":
@@ -16,6 +18,29 @@ class Enemy:
         elif self.Etype == "Legendary":
             return 5
 
+    def setHealth(self, hp): # TODO: use this : , Etype):
+        self.healthValue = hp
+
+    def impact(self, Damage):
+        self.healthValue -= Damage
+        if self.healthValue <= 0:
+            self.reDraw = False
+
+    def getHealth(self):
+        return self.healthValue
+
+    def setImageSize(self, newSize: int):
+        self.imageSize = newSize
+
+    def getImageSize(self):
+        return self.imageSize
+
+    def setReDraw(self, flag: bool):
+        self.reDraw = flag
+
+    def getReDraw(self):
+        return self.reDraw
+
     def __str__(self):
         return str(self.Etype)
 
@@ -25,6 +50,7 @@ class EnemyWave:
         self.Etype = None
         self.Size = Size
         self.wave = aDict()
+        self.initialX = 0
 
     def randomEtype(self):
         randomValue = randint(0, 1000)
@@ -37,13 +63,29 @@ class EnemyWave:
         else:
             self.Etype = "Legendary"
 
+    def setInitialX(self, iX: int):
+        self.initialX = iX
+
+    def getInitialX(self):
+        return self.initialX
+
+    def impactEnemyAtX(self, impactX, Damage):
+        if not self.wave[impactX].getReDraw():
+            return False
+        else:
+            self.wave[impactX] = self.wave[impactX].impact(Damage)
+            if not self.wave[impactX].getReDraw():
+                self.Size -= 1
+        return True
+
+    def checkSize(self):
+        return self.Size
 
     def CreateEnemyWave(self):
         self.randomEtype()
         currEnemyData = Enemy(self.Etype)
         for i in range(self.Size):
-            key = str(uuid4())
-            self.wave[key[:8]] = currEnemyData
+            self.wave[self.initialX + i*currEnemyData.imageSize] = currEnemyData
 
     def __str__(self):
         output = []
