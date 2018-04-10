75b6e3a86b5a3db30e831c2a6e9fe1723adeee65
Patrisha-de-Boon <32069658+Patrisha-de-Boon@users.noreply.github.com>
2018-04-10 00:44:48 -0600

    keyboard input works for pause screen

--- PauseScreen.py
+++ PauseScreen.py
@@ -16,7 +16,22 @@ continueGameBox = pygame.Rect(shared.width/7+85, shared.width/7+70, 190, 40)
 saveGameBox = pygame.Rect(shared.width/7+130, shared.width/7+122, 100, 40)
 menuBox = pygame.Rect(shared.width/7+130, shared.width/7+175, 100, 40)
 
-def processEvents(event):
+def colourBox(curr, colour):
+    if curr == 1:
+        pygame.draw.rect(screen, colour, continueGameBox, 3)
+    elif curr == 2:
+        pygame.draw.rect(screen, colour, saveGameBox, 3)
+    elif curr == 3:
+        pygame.draw.rect(screen, colour, menuBox, 3)
+
+
+def redrawButton(prev, curr):
+    if prev != curr:
+        buttonStrings = {1:("CONTINUE", shared.width/7+90, shared.width/7+70, 40), 2:("SAVE", shared.width/7+135, shared.width/7+122, 40), 3:("MENU", shared.width/7+135, shared.width/7+175, 40)}
+        text_to_screen(screen, buttonStrings[prev][0], buttonStrings[prev][1], buttonStrings[prev][2], buttonStrings[prev][3], shared.GOLD)
+        text_to_screen(screen, buttonStrings[curr][0], buttonStrings[curr][1], buttonStrings[curr][2], buttonStrings[curr][3], shared.WHITE)
+
+def processMouseEvents(event):
     if  continueGameBox.collidepoint(event.pos):
         text_to_screen(screen, "CONTINUE", shared.width/7+90, shared.width/7+70, 40, shared.WHITE)
         pygame.draw.rect(screen, shared.WHITE, continueGameBox , 3)
@@ -33,6 +48,28 @@ def processEvents(event):
         pygame.display.update()
         return 3
 
+def processKeyEvents(event, prev):
+    if event.key == pygame.K_RETURN:
+        return prev, True
+    curr = prev
+    if event.key == pygame.K_UP or event.key == ord('w'):
+        if prev == 1:
+            curr == 3
+        else:
+            curr -= 1
+    elif event.key == pygame.K_DOWN or event.key == ord('s'):
+        if prev == 3:
+            curr == 1
+        else:
+            curr += 1
+
+    colourBox(curr, shared.WHITE)
+    colourBox(prev, shared.GRAY)
+
+    redrawButton(prev, curr)
+
+    return curr, False
+
 # used the following resource to draw semi transparent box:
 # https://stackoverflow.com/questions/17581545/drawn-surface-transparency-in-pygame
 def drawPauseScreen():
@@ -42,13 +79,13 @@ def drawPauseScreen():
     s.fill(ck)
     s.set_colorkey(ck)
     pygame.draw.rect(s, shared.DARKBLUE, pauseScreenBox, 0)
-    s.set_alpha(100)
+    s.set_alpha(200)
     screen.blit(s, (0,0))
 
     text_to_screen(screen, "PAUSE MENU", shared.width/7+50, shared.width/7, 50, shared.GOLD)
     pygame.draw.line(screen, shared.WHITE, (shared.width/7, shared.width/7+50), (shared.width/7*6, shared.width/7+50), 2)
-    text_to_screen(screen, "CONTINUE", shared.width/7+90, shared.width/7+70, 40, shared.GOLD)
-    pygame.draw.rect(screen, shared.GRAY, continueGameBox , 3)
+    text_to_screen(screen, "CONTINUE", shared.width/7+90, shared.width/7+70, 40, shared.WHITE)
+    pygame.draw.rect(screen, shared.WHITE, continueGameBox , 3)
     text_to_screen(screen, "SAVE", shared.width/7+135, shared.width/7+122, 40, shared.GOLD)
     pygame.draw.rect(screen, shared.GRAY, saveGameBox , 3)
     text_to_screen(screen, "MENU", shared.width/7+135, shared.width/7+175, 40, shared.GOLD)
@@ -56,11 +93,12 @@ def drawPauseScreen():
 
     pygame.display.update()
 
-
 def pauseSequence():
     exit = False
     quit = False
     initPauseScreen = True
+    prev = 1
+    processed = None
     while not exit:
         if initPauseScreen:
             drawPauseScreen()
@@ -71,11 +109,20 @@ def pauseSequence():
                 exit = True
                 quit = True
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                processed = processEvents(event)
+                processed = processMouseEvents(event)
                 exit = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    processed = 1
+                    exit = True
+                else:
+                    prev, exit = processKeyEvents(event, prev)
 
     if exit and quit:
         pygame.quit()
         sys.exit()
 
-    return processed
+    if processed is not None:
+        return processed
+    else:
+        return prev
