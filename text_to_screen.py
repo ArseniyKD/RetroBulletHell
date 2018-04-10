311376d7667413da1ebc92009ea7eedd88094a86
ArseniyKD <arseniy.kouzmenkov@gmail.com>
2018-04-10 01:46:10 -0600

    put a citation on the text_to_screen function

--- text_to_screen.py
+++ text_to_screen.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 
-
+# got this function from https://stackoverflow.com/questions/20842801/how-to-display-text-in-pygame
 def text_to_screen(screen, text, x, y, size = 50,
             color = (200, 000, 000), font_type = 'images/fonts/ARCADECLASSIC.TTF'):
     try:
